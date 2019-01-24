2d3f7014fab33045ff789af072f7f1cdefd8702b
Daniel Yun <danielyun459@gmail.com>
2019-01-24 00:43:39 -0500

    UserService class added

---
+++ src/main/java/edu/neu/cs4500/services/UserService.java
@@ -0,0 +1,19 @@
+package edu.neu.cs4500.services;
+
+import edu.neu.cs4500.models.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+public class UserService {
+    static List<User> users = new ArrayList();
+
+    static {
+        users.add(new User(123, "alice", "alice", "Alice", "Wonderland"));
+        users.add(new User(234, "bob", "bob", "Bob", "Marley"));
+    }
+}
