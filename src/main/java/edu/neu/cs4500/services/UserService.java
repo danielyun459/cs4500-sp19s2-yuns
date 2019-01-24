5469a86f3cc3c0e52e3361d34d97eaad33b80b2b
Daniel Yun <danielyun459@gmail.com>
2019-01-24 00:44:31 -0500

    UserService.findAllUsers() added

--- src/main/java/edu/neu/cs4500/services/UserService.java
+++ src/main/java/edu/neu/cs4500/services/UserService.java
@@ -1,7 +1,7 @@
 package edu.neu.cs4500.services;
 
 import edu.neu.cs4500.models.User;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -16,4 +16,14 @@ public class UserService {
         users.add(new User(123, "alice", "alice", "Alice", "Wonderland"));
         users.add(new User(234, "bob", "bob", "Bob", "Marley"));
     }
+
+    @GetMapping("/api/user")
+    public List<User> findAllUsers() {
+        return users;
+    }
+
+
+
+
+
 }
