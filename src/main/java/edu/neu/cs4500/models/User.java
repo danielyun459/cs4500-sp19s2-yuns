009ad4cffd3797271248b7df2783e696b32930d0
Daniel Yun <danielyun459@gmail.com>
2019-01-24 00:36:16 -0500

    made getter/setter for User.username

--- src/main/java/edu/neu/cs4500/models/User.java
+++ src/main/java/edu/neu/cs4500/models/User.java
@@ -24,4 +24,12 @@ public class User {
     public void setId(Integer id) {
         this.id = id;
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
